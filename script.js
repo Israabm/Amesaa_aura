f986f6cf7d7700646a7ed6d0254fecab898940a0
ibenmarzouk <ibenmarzouk@vermeg.com>
2025-11-05 16:34:51 +0100

    rollback

--- script.js
+++ script.js
@@ -83,13 +83,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-// Toggle mobile menu
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
 
 
 
